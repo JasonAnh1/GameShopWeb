e137dd8c69cfe273e03c04764457fe22b5609af1
VIETANHTRAN\vietanh <vietanhtran21702@gmail.com>
2022-09-17 22:49:53 +0700

    Add project files.

---
+++ GameShop/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,56 @@
+﻿using GameShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+
+namespace GameShop.Areas.Admin.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET: Admin/DashBoard
+        game_shop_webEntities db = new game_shop_webEntities();
+        public ActionResult Index()
+        {
+            List<GameShop.Models.user> listUser = db.users.ToList(); 
+            return View(db.users.ToList());
+
+        }
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            user user = new user();
+
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult CreateUser(user user)
+        {
+            try
+            {
+                if (user.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(user.ImageUpload.FileName);
+                    string extenstion = Path.GetExtension(user.ImageUpload.FileName);
+                    fileName = fileName + extenstion;
+                    user.img_url = "~/Asset/admin/img/" + fileName;
+                    user.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Asset/admin/img/"), fileName));
+                }
+
+                user.create_at = DateTime.Now;
+                db.users.Add(user);
+                db.SaveChanges();
+                ModelState.Clear();
+
+                return RedirectToAction("Index", "DashBoard");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GameShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using GameShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        game_shop_webEntities db = new game_shop_webEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(GameShop.Models.user userModel)
+        {
+            var userDetail = db.users.Where(x => x.name==userModel.name
+            && x.password ==userModel.password)
+                .FirstOrDefault();
+            if (userDetail == null)
+            {
+                userModel.LoginErrorMessage = "Wrong username or password";
+                return View("Index",userModel);
+            }
+            if(userDetail.permission != 1)
+            {
+                userModel.LoginErrorMessage = "You dont have permission";
+                return View("Index", userModel);
+            }
+            else
+            {
+                Session["userName"] = userDetail.name;
+                return RedirectToAction("Index", "DashBoard");
+            }
+        }
+    }
+}
