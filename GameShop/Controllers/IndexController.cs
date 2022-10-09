a1b2758d0edcb17831cfb11925e6af1bb9e5794e
huubach1822 <monsterxyz02@gmail.com>
2022-10-09 17:40:37 +0700

    Index Page

---
+++ GameShop/Controllers/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using GameShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameShop.Controllers
+{
+    public class IndexController : Controller
+    {
+        game_shop_webEntities db = new game_shop_webEntities();
+        // GET: Index
+        public ActionResult Index()
+        {   
+            IndexClass banner = new IndexClass();
+            banner.banner = db.banners.Take(1).OrderByDescending(n => n.create_at).SingleOrDefault();
+            banner.bannerList = db.banners.Take(4).OrderByDescending(n => n.create_at).Skip(1).ToList();
+            banner.productList = db.products.Take(8).OrderByDescending(n => n.create_at).ToList();
+            banner.categoryList = db.categories.ToList();
+            return View(banner);
+        }
+    }
+}
---
+++ GameShop/Models/IndexClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameShop.Models
+{
+    public class IndexClass
+    {
+        public List<banner> bannerList { get; set; }
+        public banner banner { get; set; }
+        public List<product> productList { get; set; }
+        public List<category> categoryList { get; set; }
+    }
+}
